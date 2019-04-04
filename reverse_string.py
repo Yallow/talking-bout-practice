149d70bd93bea9243c7aa839d44223cb5b9ae9db
Yallow <tylerbouma1@gmail.com>
2019-04-04 20:26:58 +0000

    Reverse an input string

---
+++ reverse_string.py
@@ -0,0 +1,13 @@
+def main():
+    reverse_string("madman")
+
+def reverse_string(a_string):
+    # new array for reversed string
+    str_rev = []
+    # walk through the string like an array
+    for c in list(a_string):
+    str_rev.insert(0, c)
+
+    return "".join(str_rev)
+
+main()
