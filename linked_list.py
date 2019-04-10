f839068895f41dac513fd1daa029a1ce35290d02
Tyler Bouma <tylerbouma1@gmail.com>
2019-04-10 13:38:07 -0500

    Added sort_list
    Uses bubble sorting at the moment

--- linked_list.py
+++ linked_list.py
@@ -16,6 +16,14 @@ class LinkedList:
 	def __init__(self, head=None):
 		self.head = head
 
+	def size(self):
+		node = self.head
+		counter = 0
+		while node != None:
+			counter+=1
+			node = node.get_next()
+		return counter
+
 	def traverse(self):
 		node = self.head
 		while node != None:
@@ -36,9 +44,35 @@ class LinkedList:
 		while c_node.get_next():
 			c_node = c_node.get_next()
 		c_node.set_next(new_node)
+
+	def sort_list(self, size):
+		# use a bubble sort for this at the moment
+		node = self.head
+		l_length = size
+		for _ in range(l_length):
+			swapped = False
+			node = self.head
+			while node != None and node.get_next() != None:	
+				if node.get_data() > node.get_next().get_data():
+					# swap em
+					holder = node.get_next().data
+					node.get_next().data = node.data
+					node.data = holder
+					swapped = True
+				node = node.get_next()
+				
+			if swapped == False:
+				# we are done sorting
+				return
+
+
 		
 new_l = LinkedList()
 new_l.head = Node(22)
 new_l.insert_end(11)
 new_l.insert_front(44)
+new_l.insert_end(200)
+new_l.insert_end(2)
+new_l.traverse()
+new_l.sort_list(new_l.size())
 new_l.traverse()
