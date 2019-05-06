b6936c089cbae7f804a7637b5b36904259b4cac0
Tyler Bouma <tylerbouma1@gmail.com>
2019-05-06 15:00:26 -0500

    Find the if the tree is BST (binary search tree)

---
+++ bst_check.py
@@ -0,0 +1,33 @@
+import collections
+import sys
+# Collections module has already been imported.
+class BinaryTree:
+    def __init__(self, root_node = None):
+            self.root = root_node
+
+    
+    def validate_BST_Itr(self,root):
+        class TreeBoundryNode:
+            def __init__(self, tree_node = None, left_boundry = None, right_boundry = None):
+                self.tree_node = tree_node
+                self.left_boundry = left_boundry
+                self.right_boundry = right_boundry
+
+        if root == None or (root.left_child == None and root.right_child == None):
+            return True
+        
+        queue = collections.deque()
+        queue.append(TreeBoundryNode(root,-sys.maxint-1, sys.maxint))
+        while queue:
+            tree_binary_node = queue.popleft()
+            tree_node = tree_binary_node.tree_node
+            if (tree_node.data <= tree_binary_node.left_boundry) or (tree_node.data >= tree_binary_node.right_boundry):
+                return False
+
+            if tree_node.left_child != None:
+                queue.append(TreeBoundryNode(tree_node.left_child, tree_binary_node.left_boundry, tree_node.data))
+
+            if tree_node.right_child != None:
+                queue.append(TreeBoundryNode(tree_node.right_child, tree_node.data, tree_binary_node.right_boundry))
+
+            return True
