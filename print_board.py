358355f66cd19b5084c6c0bb867db2f71d6456ba
Tyler Bouma <tylerbouma1@gmail.com>
2019-05-14 07:58:55 -0500

    Add files via upload
    DFS using recursion.

--- print_board.py
+++ print_board.py
@@ -6,18 +6,27 @@ def print_paths(board):
     return path_list
 
 def search(i, j, board, temp_str_list, path_list):
+    # y axis of board
     rows = len(board)
+    # x axis of board
     cols = len(board[0])
-    if i > rows -1 or j > cols - 1:
+    # we've reached the end of the board (either vertically or horizontally)
+    if i > rows - 1 or j > cols - 1:
         return
+    # add the letter to the temp list 
     temp_str_list.append(board[i][j])
+    # we have reached the bottom right corner
     if i == rows - 1 and j == cols - 1:
         path_list.append("".join(temp_str_list))
+        # get rid of this list item now
         temp_str_list.pop()
         return
 
+    # traverse vertically first
     search(i+1, j, board, temp_str_list, path_list)
+    # traverse horizontally second
     search(i, j+1, board, temp_str_list, path_list)
+    # get rid of anything remaining
     temp_str_list.pop()
 
 temp = print_paths([['A','X','E'],['B','Z','Q']])
